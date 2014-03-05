3dcd33fbc38f854959270b24c49c7288790271db
elauqsap <pdago@icloud.com>
2014-03-05 15:33:36 -0600

    fix conf

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	spotify_name: '3lauqsap',
-	spotify_pass: 'Spotify.pdagosti34!',
-    port: 3000,
-    sessionSecret: 'hawkeye-secret-token',
-    cookieSecret: 'hawkeye-secret-token',
-    cookieMaxAge: (1000 * 60 * 60 * 24 * 365)
-};
