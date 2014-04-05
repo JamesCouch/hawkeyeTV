06ca4f47ebe42025a03f26175c6f2c49e082caee
James <james-couch@uiowa.edu>
2014-04-05 15:28:32 -0500

    add fb and tw login/logout

--- server.js
+++ server.js
@@ -2,21 +2,19 @@
 var express = require('express'),
     http = require('http'),
     bcrypt = require("bcrypt"),
-    
+
     // Setup the audio stream decoder, system audio speaker,
     // and the passthrough stream for the audio passed from spotify
     Lame = require('lame'),
     lame = new Lame.Decoder(),
     Speaker = require('speaker'),
     spkr = new Speaker(),
-    Stream = require('stream'),
-    stream = new Stream(),
-    
+
     // Initialize the spotify search engine and the spotify-web
     // interface that will pull track info and streams
     spotify = require('spotify-web'),
     search_spotify = require('spotify'),
-    
+
     // Load the config file with app settings
     config = require("./config"),
 
@@ -29,12 +27,10 @@ var express = require('express'),
     _ = require("underscore"),
     omx = require('omxcontrol'),
     exec = require('child_process').exec,
-    Twit = require('twit'),    
+    Twit = require('twit'),
 
     app = express(),
     server = http.createServer(app).listen( process.env.PORT || config.port );
-var uri;
-
 
     // Initialize sqlite and create our db if it doesnt exist
 var sqlite3 = require("sqlite3").verbose();
@@ -43,41 +39,10 @@ var Twit = require('twit');
 var feed = require("feed-read");
 var FB = require("fb");
 
-
-    // spotify.login( config.spotify_name, config.spotify_pass, function (err, spotify) {
-    //   if (err) throw err;
-    //   // first get a "Track" instance from the track URI
-    //   spotify.get(uri, function (err, track) {
-    //     if (err) throw err;
-    //     console.log('Playing: %s - %s', track.artist[0].name, track.name);
-
-    //     // play() returns a readable stream of MP3 audio data
-    //     stream = track.play();
-    //     stream
-    //       .pipe(lame)
-    //       .pipe(spkr)
-    //       .on('finish', function () {
-    //         spotify.disconnect();
-    //       });
-    //     setTimeout(function (){
-    //         stream.unpipe(lame.unpipe(spkr.end()));
-    //         console.log("**************** AFTER UNPIPE ******************");
-    //         stream.pause();
-    //     }, 5000);
-    //     setTimeout(function (){
-    //         console.log("**************** REPIPE ******************");
-    //         spkr = new Speaker();
-    //         stream.pipe(lame).pipe(spkr).on('finish', function () {
-    //             spotify.disconnect();
-    //         });
-    //   }, 9000);
-    //   });
-    // });
-
-
 var io = require('socket.io').listen(server);
 var views = ['chrome','youtube','settings', 'music', 'facebook', 'twitter', 'news', 'home'];
-var ss;
+var ss, stream;
+var playback = false;
 var ms;
 
 var passport = require('passport')
@@ -94,7 +59,7 @@ var passport = require('passport')
         db.run("UPDATE profiles SET tw_token = ?, tw_secret =? WHERE id = ?", [ token, tokenSecret, "1" ], function(err){
             if(err) {
                 console.log("Failed to load TW oauth token in the database");
-            } 
+            }
         });
         done(null);
     }
@@ -110,7 +75,7 @@ passport.use(new FacebookStrategy({
         db.run("UPDATE profiles SET fb_token = ?, fb_refresh =? WHERE id = ?", [ accessToken, refreshToken, "1" ], function(err){
             if(err) {
                 console.log("Failed to load FB oauth token in the database");
-            } 
+            }
         });
         done(null);
     }
@@ -119,7 +84,7 @@ passport.use(new FacebookStrategy({
  //Socket.io Server
  io.set('log level', 1);
  io.sockets.on('connection', function (socket) {
-    
+
     socket.on("screen", function(data) {
         socket.type = "screen";
         ss = socket;
@@ -169,156 +134,104 @@ passport.use(new FacebookStrategy({
 
         feed(data, function(err, articles) {
           if (err) throw err;
-
           ss.emit('rss-control',articles);
           // Each article has the following properties:
-          // 
+          //
           //   * "title"     - The article title (String).
           //   * "author"    - The author's name (String).
           //   * "link"      - The original article link (String).
           //   * "content"   - The HTML content of the article (String).
           //   * "published" - The date that the article was published (Date).
           //   * "feed"      - {name, source, link}
-          // 
+          //
         });
-
-
-
     });
 
+    socket.on("get-facebook-status",function(data){
+
+         db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
+
+             var token = user.fb_token;
+             var secret = user.fb_refresh;
+
+             if(typeof ms != "undefined"){
+                 if(token === null){
+
+                      ms.emit('sent-facebook-status',"logged_out");
+                 }
+                 else{
+                      ms.emit('sent-facebook-status',"logged_in");
+                  }
+             }
+         });
+     });
+    socket.on("get-twitter-status",function(data){
+
+            db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
+
+                console.log("tw-token");
+                console.log(user.tw_token);
+
+                var token = user.tw_token;
+                var secret = user.tw_secret;
+
+                if(typeof ms != "undefined"){
+                    if(token === null){
+
+                         ms.emit('sent-twitter-status',"logged_out");
+                    }
+                    else{
+                         ms.emit('sent-twitter-status',"logged_in");
+                     }
+                }
+            });
+        });
+
+     socket.on("log-out-facebook",function(data){
+
+         db.run("UPDATE profiles SET fb_token = ?, fb_refresh =? WHERE id = ?", [ null, null, "1" ], function(err){
+             if(err) {
+                 console.log("Failed to load TW oauth token in the database");
+             } 
+         });
+         ss.emit('log-out-facebook');
+         if(typeof ms != "undefined"){
+             ms.emit('sent-facebook-status-status',"logged_out");
+         }
+     });
+
+     socket.on("log-out-twitter",function(data){
+         db.run("UPDATE profiles SET tw_token = ?, tw_secret =? WHERE id = ?", [ null, null, "1" ], function(err){
+             if(err) {
+                 console.log("Failed to load TW oauth token in the database");
+             } 
+         });
+         ss.emit('log-out-twitter');
+         if(typeof ms != "undefined"){
+             ms.emit('sent-twitter-status',"logged_out");
+         }
+     });
 
     socket.on("log-in-facebook", function(data) {
             console.log("log in fb");
             ss.emit('ss-log-in-facebook', data);
     });
 
-    socket.on("get-facebook-status",function(data){
+    socket.on("on-click-facebook", function(data) {
+      db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
+        var token = user.fb_token;
+        var secret = user.fb_refresh;
 
-            db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
-
-                var token = user.fb_token;
-                var secret = user.fb_refresh;
-
-                if(typeof ms != "undefined"){
-
-                    if(token === null){
-
-                         ms.emit('sent-facebook-status',"logged_out");
-
-                    }
-                    else{
-                         ms.emit('sent-facebook-status',"logged_in");
-
-                     }
-                }
-
-            });
-
-
-        });
-
-        socket.on("log-out-facebook",function(data){
-
-            db.run("UPDATE profiles SET fb_token = ?, fb_refresh =? WHERE id = ?", [ null, null, "1" ], function(err){
-                if(err) {
-                    console.log("Failed to load TW oauth token in the database");
-                } 
-            });
-            ss.emit('log-out-facebook');
-
-
-            if(typeof ms != "undefined"){
-
-                ms.emit('sent-facebook-status-status',"logged_out");
-
-            }
-
-
-        });
-
-
-        socket.on("on-click-facebook", function(data) {
-
-
-            db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
-
-
-                var token = user.fb_token;
-                var secret = user.fb_refresh;
-
-                if(token === null){
-                     ss.emit('facebook-login');
-
-                }
-                else{
-
-                    FB.setAccessToken(token);
-
-                    FB.api('/me/home', function(r) { 
-
-                        ss.emit('sent-facebook-feed',r);
-
-
-                    })
-
-                 }
-
-
-            });
-
-
+        if(token === null){
+          ss.emit('facebook-login');
+        } else{
+            FB.setAccessToken(token);
+            FB.api('/me/home', function(r) {
+              ss.emit('sent-facebook-feed',r);
+            })
+          }
+      });
     });
-
-        socket.on("log-out-twitter",function(data){
-
-            db.run("UPDATE profiles SET tw_token = ?, tw_secret =? WHERE id = ?", [ null, null, "1" ], function(err){
-                if(err) {
-                    console.log("Failed to load TW oauth token in the database");
-                } 
-            });
-
-            ss.emit('log-out-twitter');
-
-            if(typeof ms != "undefined"){
-
-                ms.emit('sent-twitter-status',"logged_out");
-
-
-            }
-
-
-        });
-
-
-        socket.on("get-twitter-status",function(data){
-
-        db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
-
-            console.log("tw-token");
-            console.log(user.tw_token);
-
-            var token = user.tw_token;
-            var secret = user.tw_secret;
-
-            if(typeof ms != "undefined"){
-
-                if(token === null){
-
-                     ms.emit('sent-twitter-status',"logged_out");
-
-                }
-                else{
-                     ms.emit('sent-twitter-status',"logged_in");
-
-                 }
-            }
-
-        });
-
-
-    });
-
-
 
     socket.on("play-youtube", function(data) {
         console.log(data.id);
@@ -335,13 +248,82 @@ passport.use(new FacebookStrategy({
             });
     });
 
+    socket.on("spotify-search", function(data) {
+      console.log("Spotify - Search: ", data);
+      search_spotify.search({ type: 'track', query: data.query }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else if ( data.info.num_results == 0 ) {
+          ss.broadcast.emit('spotify-null');
+        } else {
+          tracks = {};
+          loop = 5;
+          if (data.info.num_results < 6) {
+            console.log(data.info.num_results);
+            loop = data.info.num_results;
+          }
+          // NEED to allow results of different sizes
+          for (i = 0; i < loop; i++){
+            key = i;
+            min = parseInt(data.tracks[i].length / 60) % 60;
+            sec = Math.floor(data.tracks[i].length % 60);
+            tracks[key] = { name: data.tracks[i].name,
+              artist: data.tracks[i].artists[0].name,
+              href: data.tracks[i].href,
+              time: min + ":" + (sec < 10 ? "0"+sec : sec)
+            }
+          }
+          tracks['size'] = loop;
+          ss.broadcast.emit('spotify-results', tracks);
+        }
+        });
+    });
+
+    socket.on("spotify-play", function(data) {
+      if (stream != null) {
+        if (stream._readableState.flowing) {
+          console.log("Spotify - Second Play");
+          stopSpotify();
+        }
+        console.log("HERE");
+        stream = null;
+        spotifyPlay(data);
+        ss.broadcast.emit('spotify-reset');
+      } else {
+        console.log("Spotify - First Play");
+        spotifyPlay(data);
+      }
+    });
+    socket.on("spotify-pause", function(data) {
+      if (stream != null ){
+        if (stream._readableState.flowing) {
+          stopSpotify();
+          stream.pause();
+        } else if (stream._readableState.pipesCount == 0) {
+            stopSpotify();
+            stream.pause();
+        }
+      }
+    });
+    socket.on("spotify-resume", function(data) {
+      if (stream != null ){
+        if (!stream._readableState.flowing) {
+          console.log("Spotify - Resume");
+          stream.resume();
+          stream
+            .pipe(lame)
+            .pipe(spkr);
+        }
+      }
+    });
 
     socket.on("on-click-twitter", function(data) {
 
         // we need to check if they have a token and token secret
         //if they do have those,  we create an object out of it. We should do this on server start
         //When the user clicks on the twitter button, and there is no object, we redirect them to login
-        //if there is an object, we send them data. 
+        //if there is an object, we send them data.
 
         db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, user){
             if(user.tw_token === null){
@@ -362,6 +344,36 @@ passport.use(new FacebookStrategy({
         });
     });
  });
+
+function spotifyPlay(data) {
+  spotify.login(config.spotify_name, config.spotify_pass, function (err, spotify) {
+    if (err) throw err;
+    // first get a "Track" instance from the track URI
+    spotify.get(data.track, function (err, track) {
+      if (err) throw err;
+      stream = track.play();
+      stream
+        .pipe(lame)
+        .pipe(spkr)
+        .on('finish', function () {
+          spotify.disconnect();
+        });
+    });
+  });
+}
+
+function stopSpotify() {
+  console.log("Spotify - Stop");
+  if ( stream != null ) {
+    stream.unpipe(lame);
+  }
+  if ( lame != null) {
+    lame.unpipe(spkr.end());
+  }
+  spkr, lame = null;
+  lame = new Lame.Decoder()
+  spkr = new Speaker();
+}
 
 // Create our profiles table if it doesn't exist
 db.run("CREATE TABLE IF NOT EXISTS profiles (id INTEGER PRIMARY KEY, zipcode TEXT, news TEXT, theme TEXT, tw_token TEXT, tw_secret TEXT, fb_token TEXT, fb_refresh TEXT )");
@@ -386,7 +398,7 @@ app.use( express.methodOverride() );
 app.use( express.bodyParser() );            // Needed to parse POST data sent as JSON payload
 
 // Cookie config
-// 
+//
 app.use( express.cookieParser( config.cookieSecret ) );           // populates req.signedCookies
  app.use( express.cookieSession( config.sessionSecret ) );         // populates req.session, needed for CSRF
 
@@ -407,10 +419,10 @@ app.get("/", function(req, res){
 db.serialize(function(){
     db.get("SELECT * FROM profiles WHERE id = ?", [ "1" ], function(err, profile){
         if(profile){
-            console.log("DB has first record");  
-        } else {  
-            db.run("INSERT INTO profiles (zipcode, news, theme) VALUES (?,?,?)", 
-                [ "52242", "disabled", "default"], function(err){ 
+            console.log("DB has first record");
+        } else {
+            db.run("INSERT INTO profiles (zipcode, news, theme) VALUES (?,?,?)",
+                [ "52242", "disabled", "default"], function(err){
                 if(err){
                     console.log(err);
                 }
@@ -422,25 +434,25 @@ db.serialize(function(){
 app.post("/api/auth/open", function(req, res){
     db.get("SELECT * FROM profiles WHERE id = ?", [ req.body.id ], function(err, profile){
     if(profile){
-        res.json({ profile: profile });   
-    } else {  
-        res.json({ error: "Default profile" });   
+        res.json({ profile: profile });
+    } else {
+        res.json({ error: "Default profile" });
     }
     });
 });
 
 app.post("/api/auth/update", function(req, res){
     db.serialize(function(){
-        db.run("UPDATE profiles SET zipcode = ?, news = ?, theme = ? WHERE id = ?", 
+        db.run("UPDATE profiles SET zipcode = ?, news = ?, theme = ? WHERE id = ?",
             [ req.body.zipcode, req.body.news, req.body.theme, req.body.id ], function(err){
             if(err) {
                 console.log("Updating the user failed");
             } else {
                 db.get("SELECT * FROM profiles WHERE id = ?", [ req.body.id ], function(err, profile){
                     if(profile){
-                        res.json({ profile: profile });   
-                    } else {  
-                        res.json({ error: "Default profile" });   
+                        res.json({ profile: profile });
+                    } else {
+                        res.json({ error: "Default profile" });
                     }
                 });
             }
@@ -465,9 +477,10 @@ app.post("/api/auth/play", function(){
       // first get a "Track" instance from the track URI
       spotify.get(uri, function (err, track) {
         if (err) throw err;
-        track.play()
-          .pipe(new lame.Decoder())
-          .pipe(new Speaker())
+        stream = track.play();
+        stream
+          .pipe(lame)
+          .pipe(spkr)
           .on('finish', function () {
             spotify.disconnect();
           });
@@ -477,18 +490,18 @@ app.post("/api/auth/play", function(){
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
 
-app.get('/auth/twitter/callback', 
+app.get('/auth/twitter/callback',
   passport.authenticate('twitter', { successRedirect: '/',
                                      failureRedirect: '/' }));
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback', 
+app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/',
                                      failureRedirect: '/' }));
 
 
-// Close the db connection on process exit 
+// Close the db connection on process exit
 // (should already happen, but to be safe)
 process.on("exit", function(){
     db.close();
