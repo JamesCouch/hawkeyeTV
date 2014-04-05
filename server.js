f55816a5c742852d5f93877e6d348cbb12dce7c8
James <james-couch@uiowa.edu>
2014-04-05 16:11:21 -0500

    added fb settings change

--- server.js
+++ server.js
@@ -494,7 +494,7 @@ app.get('/auth/twitter/callback',
   passport.authenticate('twitter', { successRedirect: '/',
                                      failureRedirect: '/' }));
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook', passport.authenticate('facebook',{scope: 'read_stream'}));
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/',
